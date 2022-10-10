5bf0c1d19b5dd651cfe95fa928587e9277dc8800
stalingeorg <stalingeorge46@gmail.com>
2022-10-10 14:12:27 +0530

    update on oct 10 2022

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import {BiSearch} from 'react-icons/bi'
+import {BsPerson} from 'react-icons/bs'
+import {BsBorderWidth} from 'react-icons/bs'
+
+import './NavbarStyles.css'
+
+function Navbar() {
+    const [nav, setNav] = useState(false)
+    const handleNav = () => setNav(!nav)
+  return (
+    <div className='navbar'>
+        <div className='logo'>
+<h2>Travels</h2>
+        </div> 
+        <ul className='nav-menu'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Resources</li>
+            <li>Contact</li>
+        </ul>
+<div className='nav-icons'>
+    <BiSearch className="icon"></BiSearch>
+    <BsPerson className="icon"></BsPerson>
+</div>
+<div className='hamburger'>
+    <BsBorderWidth className="icon"></BsBorderWidth>
+</div>
+<div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
+<ul className='mobile-nav'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Resources</li>
+            <li>Contact</li>
+        </ul>
+</div>
+    </div>
+  )
+}
+
+export default Navbar
