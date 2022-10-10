4b6b6d3be9c1eef19f502bcb85315a7738fd00d5
stalingeorg <stalingeorge46@gmail.com>
2022-10-10 17:43:36 +0530

    bootstrap added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,43 +1,36 @@
-import React, { useState } from 'react'
-import {BiSearch} from 'react-icons/bi'
-import {BsPerson} from 'react-icons/bs'
-import {BsBorderWidth} from 'react-icons/bs'
-import { Button } from 'react-bootstrap'
+import  Container  from 'react-bootstrap/Container'
+import  Nav  from 'react-bootstrap/Nav'
+import NavbarBootstrap from 'react-bootstrap/Navbar'
+import  NavDropdown  from 'react-bootstrap/NavDropdown'
 
 import './NavbarStyles.css'
 
-function Navbar() {
-    const [nav, setNav] = useState(false)
-    const handleNav = () => setNav(!nav)
+function Navbar() { 
   return (
-    <div className='navbar'>
-        <div className='logo'>
-<h2>Travels</h2>
-        </div> 
-        <ul className='nav-menu'>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Resources</li>
-            <li>Contact</li>
-            <li><Button variant="primary">Primary</Button>{' '}</li>
-        </ul>
-<div className='nav-icons'>
-    <BiSearch className="icon"></BiSearch>
-    <BsPerson className="icon"></BsPerson>
-</div>
-<div className='hamburger' onClick={handleNav}>
-    <BsBorderWidth className="icon"></BsBorderWidth>
-</div>
-<div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
-<ul className='mobile-nav'>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Resources</li>
-            <li>Contact</li>
-        </ul>
-</div>
-    </div>
+    <NavbarBootstrap bg="light" expand="lg">
+      <Container fluid>
+        <NavbarBootstrap.Brand href="#home">React-Bootstrap</NavbarBootstrap.Brand>
+        <NavbarBootstrap.Toggle aria-controls="basic-navbar-nav" />
+        <NavbarBootstrap.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </NavbarBootstrap.Collapse>
+      </Container>
+    </NavbarBootstrap>
   )
 }
 
-export default Navbar
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
  import "../node_modules/react-bootstrap/dist/react-bootstrap";
- import "../node_modules/bootstrap/dist/css/bootstrap";
- import "../node_modules/bootstrap/dist/css/bootstrap";
+ import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
